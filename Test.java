26f37095bde95c527b5bc7e24e903b9eb6f5eebb
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-05-27 08:45:54 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Test
+{
+    public static void main(String args[])
+    {
+       int year;
+     Scanner in=new Scanner(System.in);
+     System.out.println("Enter the value of year");
+      
+        // If a year is multiple of 400,
+        // then it is a leap year
+        if (year % 400 == 0)
+            return true;
+     
+        // Else If a year is multiple of 100,
+        // then it is not a leap year
+        if (year % 100 == 0)
+            return false;
+     
+        // Else If a year is multiple of 4,
+        // then it is a leap year
+        if (year % 4 == 0)
+            return true;
+        return false;
+    }
+         
+    // Driver method
+    public static void main(String[] args)
+    {
+        int year = 2000;
+        System.out.println( checkYear(2000)? "Leap Year" :
+                           "Not a Leap Year" );
+    }
+}
