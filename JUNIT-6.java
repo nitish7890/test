26687fb885d8b0fb0282f5f7b2b53e07d990f3fa
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-05-29 09:31:08 +0530

    Add files via upload

---
+++ JUNIT-6.java
@@ -0,0 +1,17 @@
+import java.lang.Math;
+  
+class Gfg1 {
+  
+    // driver code
+    public static void main(String args[])
+    {
+  
+        int l = 10;
+        // returns the string representation of the unsigned int value
+        // represented by the argument in binary (base 2)
+        System.out.println("Binary is " + Integer.toBinaryString(l));
+  
+        l = 9;
+        System.out.println("Binary is " + Integer.toBinaryString(l));
+    }
+}
