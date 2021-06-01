8203cbdb3d6f1cd56122c6eab324eb2154791ef2
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-06-01 09:00:06 +0530

    Add files via upload

--- JUNIT-6.java
+++ JUNIT-6.java
@@ -1,17 +1,24 @@
-import java.lang.Math;
-  
-class Gfg1 {
-  
-    // driver code
-    public static void main(String args[])
-    {
-  
-        int l = 10;
-        // returns the string representation of the unsigned int value
-        // represented by the argument in binary (base 2)
-        System.out.println("Binary is " + Integer.toBinaryString(l));
-  
-        l = 9;
-        System.out.println("Binary is " + Integer.toBinaryString(l));
-    }
+import java.util.*;
+class BinToDec
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int N;
+int A[]=new int[20];
+System.out.println("Enter the Binary number");
+N=in.nextInt();
+int i=0;
+while(N>0)
+{
+A[i]=N%8;
+N=N/8;
+i++;
+}
+System.out.print("Decimal Equivalent=");
+for(i=i-1;i>=0;i--)
+{
+System.out.print(A[i]);
+}
+}
 }
