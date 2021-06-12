0d8ef1dbdcd3bac648f27e5251bf9ff44bcf73f7
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-06-12 09:36:02 +0530

    Update TicTacGame.java

--- TicTacGame.java
+++ TicTacGame.java
@@ -1,15 +1,17 @@
-public class TicTacGame {
-	
-	    char[] board=new char[10];
-		public  void createBoard() {  
+
+package com.brizdgelabz.tictactoegame;
+
+public class TicTacToeGame {  //Declare the name of class TicTacToeGame
+	char[] board=new char[10]; //Declare the char type array its size is 10
+		public  void createBoard() {  // Declare the method
 		for(int i=1;i<board.length;i++) {
 			board[i]=' ';
 		}
 		}	
-	public static void main(String args[]) {
-		TicTacGame ob=new TicTacGame();
+	public static void main(String args[]) {  //main method
+		TicTacToeGame tt=new TicTacToeGame();  //Define the object
 		System.out.println("welcome in TicTacGame");
-		    ob.createBoard();
+		    tt.createBoard();   //calling the method createBoard
 	}
 
 }
